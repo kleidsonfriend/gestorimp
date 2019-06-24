2124844181fb3ee352f595219881fa403d8be729
Kleidson da Silva <kleidsonfriend@hotmail.com>
2019-06-24 16:38:19 -0300

    versão estável

---
+++ resources/lang/pt-br/app.php
@@ -0,0 +1,78 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Label language files for All Module
+    |--------------------------------------------------------------------------
+    |
+     */
+
+    'name'           => 'Lavalite',
+    'name.html'      => '<b>Lava</b>lite',
+    'name.short'     => '<b>L</b>l',
+    'admin.panel'    => 'Admin Panel',
+    'dashboard'      => 'Dashboard',
+    'rights'     => '<a href="/" target="_blank">GestorIMP</a> &copy; 2019. All Rights Reserved.
+    Powered by <a href="https://w3bfree.com/" target="_blank">W3Bfree</a>',
+    'version'        => '<b>Version</b> Develop',
+
+    'add'            => 'Add',
+    'addnew'         => 'Add new',
+    'actions'        => 'Actions',
+    'approve'        => 'Approve',
+    'archive'        => 'Archived',
+    'unarchive'      => 'Unarchived',
+    'back'           => 'Back',
+    'cancel'         => 'Cancel',
+    'close'          => 'Close',
+    'copy'           => 'Copy',
+    'complete'       => 'Completed',
+    'create'         => 'Create',
+    'dashboard'      => 'Dashboard',
+    'delete'         => 'Delete',
+    'draft'          => 'Draft',
+    'details'        => 'Details',
+    'edit'           => 'Edit',
+    'enterkeyword'   => 'Please enter a keword to search',
+    'go'             => 'Go',
+    'help'           => 'Help',
+    'home'           => 'Home',
+    'list'           => 'List',
+    'logout'         => 'Logout',
+    'logs'           => 'Logs',
+    'manage'         => 'Manage',
+    'more'           => 'More',
+    'new'            => 'New',
+    'no'             => 'No',
+    'opt'            => 'Options',
+    'option'         => 'Option',
+    'options'        => 'Options',
+    'order'          => 'Order',
+    'password'       => 'Password',
+    'profile'        => 'Profile',
+    'publish'        => 'Published',
+    'request'        => 'Request',
+    'reports'        => 'Reports',
+    'reset'          => 'Reset',
+    'save'           => 'Save',
+    'search'         => 'Search',
+    'settings'       => 'Settings',
+    'show'           => 'Show',
+    'status'         => 'Status',
+    'update'         => 'Update',
+    'update_profile' => 'Update Profile',
+    'unpublish'      => 'Unpublished',
+    'view'           => 'View',
+    'verify'         => 'Verifyed',
+    'yes'            => 'Yes',
+
+    'flash'          => [
+        'error'   => 'Success',
+        'error'   => 'Error',
+        'warning' => 'Warning',
+        'info'    => 'Info',
+    ],
+
+];
